50a015ffc297d89522d525ecbc3f5ba4233ff197
oengajohn <johnongwae4@gmail.com>
2023-04-28 13:35:52 +0300

    Feat: Implementation of circuit breaker pattern
    -add resilience 4j dependencies

--- fundmaster/administration/user-service/src/main/java/com/systech/userservice/controller/UserController.java
+++ fundmaster/administration/user-service/src/main/java/com/systech/userservice/controller/UserController.java
@@ -2,6 +2,7 @@ package com.systech.userservice.controller;
 
 import com.systech.userservice.dto.UserRequestDto;
 import com.systech.userservice.dto.UserResponseDto;
+import com.systech.userservice.dto.toExtResponse;
 import com.systech.userservice.service.UserService;
 import java.util.Collections;
 import java.util.List;
@@ -12,6 +13,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
@@ -23,19 +25,30 @@ public class UserController {
     private final UserService userService;
 
     @GetMapping
-    public List<UserResponseDto> getAllUSer(){
+    public List<UserResponseDto> getAllUSer() {
         return userService.getAllUsers();
     }
 
     @PostMapping
-    public boolean createUser(@RequestBody UserRequestDto userRequestDto){
+    public boolean createUser(@RequestBody UserRequestDto userRequestDto) {
         //service
         return userService.userService(userRequestDto);
 
     }
+
     @GetMapping("/{userId}")
-    public UserResponseDto getUserWithSchemeInformation(@PathVariable("userId") Long userId) throws Exception {
-        return userService.getUserById(userId);
+    @CircuitBreaker(name = "schemeservice", fallbackMethod = "schemeServiceUnavailable")
+
+    public toExtResponse getUserWithSchemeInformation(@PathVariable("userId") Long userId) throws Exception {
+        UserResponseDto userById = userService.getUserById(userId);
+       return toExtResponse.builder().success(true).msg("Details ok").userResponseDto(userById).build();
     }
+
+    public toExtResponse schemeServiceUnavailable(Long userId,
+                                                    RuntimeException runtimeException) {
+        return toExtResponse.builder().success(false).msg("Oops! Service taking too long to respond...please try again later").build();
+
+    }
+
 
 }
---
+++ fundmaster/administration/user-service/src/main/java/com/systech/userservice/dto/toExtResponse.java
@@ -0,0 +1,20 @@
+package com.systech.userservice.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@Builder
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
+public class toExtResponse {
+    private UserResponseDto userResponseDto;
+    private boolean success;
+    private String msg;
+}
